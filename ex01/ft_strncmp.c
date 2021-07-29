628add180ff29a8e0e8313c90c7d5c95c00a21a6
sdxsd <willmaguire@icloud.com>
2021-07-29 13:55:37 +0200

    Gentoo

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -19,12 +19,10 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 /*
 int main()
 {
-	char *str1 = "";
-	char *str2 = "a";
+	char *str1 = "AB";
+	char *str2 = "AC";
 	char *str3 = "jefkjjjjdj";
-	printf("%d\n", ft_strncmp(str1, str2, 5));
-	printf("%d\n", ft_strncmp(str2, str3, 5));
-	printf("%d\n\n", ft_strncmp(str3, str1, 5));
+	printf("%d\n", ft_strncmp(str1, str2, 2));
 	printf("%d\n", strncmp(str1, str2, 5));
 	printf("%d\n", strncmp(str2, str3, 5));
 	printf("%d\n", strncmp(str3, str1, 6));
